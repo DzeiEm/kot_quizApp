5b078b6d06ddac3fe9e5fd4fecd10c780c9e3575
DzeiEm <julija.melesina@gmail.com>
2020-06-02 15:27:29 +0300

    updates

--- app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
@@ -3,6 +3,8 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_question.*
+import kotlinx.android.synthetic.main.activity_quiz_question.view.*
 import javax.security.auth.login.LoginException
 
 class QuizQuestionActivity : AppCompatActivity() {
@@ -12,5 +14,22 @@ class QuizQuestionActivity : AppCompatActivity() {
 
         val questionsList = Constants.getQuestions()
         Log.i("Question size","${questionsList.size}")
+
+        val currentPosition = 1
+        val question: Question = questionsList[currentPosition - 1]
+
+        progressBarLine.progress = currentPosition
+        progressBarOutOf.text  = "$currentPosition" + "/" + "${progressBarLine.max}"
+
+        questionTitle.text = question.question
+        image.setImageResource(question.image)
+
+        questionOptionOne.text  = question.optionOne
+        questionOptionTwo.text  = question.optionTwo
+        questionOptionThree.text  = question.optionThree
+        questionOptionFour.text  = question.optionFour
+
+
+
     }
 }
